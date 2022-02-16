5db8a7e6ee09f5f444c7dc86bf43df82179f0735
mr_str <80831520+motorin-romchik@users.noreply.github.com>
2022-02-16 18:27:41 +0700

    Initial commit

---
+++ src/ParmDemo.java
@@ -0,0 +1,15 @@
+class ChkNum{
+    boolean isEven (int x) {
+        if ((x%2) == 0) return true;
+        else return false;
+    }
+}
+public class ParmDemo {
+    public static void main (String args[]) {
+        ChkNum e = new ChkNum();
+
+        if (e.isEven(10)) System.out.println("10 - четное число ");
+        if (e.isEven(9)) System.out.println("9 - четное число");
+        if (e.isEven(8)) System.out.println("8 - четное число");
+    }
+}
